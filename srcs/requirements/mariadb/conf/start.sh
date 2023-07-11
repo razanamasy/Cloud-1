df691bccba7b235be86fb3c730bc66cf1183bb3a
razanamasy <hina.razanamasy@gmail.com>
2023-07-11 17:06:17 +0200

    add user phpmyadmin in mariadb, add right pass and user var in phpmyadmin env, add skip grant in 50-server.cnf but not sur it is mandatory

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -10,6 +10,8 @@ CREATE DATABASE IF NOT EXISTS hina_db ;
 SHOW DATABASES ;
 CREATE USER "hina"@"wordpress.srcs_my_network" IDENTIFIED BY "$MYSQL_DB_PASS";
 GRANT ALL PRIVILEGES ON hina_db.* TO "hina"@"wordpress.srcs_my_network" IDENTIFIED BY "$MYSQL_DB_PASS";
+CREATE USER "hina"@"phpmyadmin.srcs_my_network" IDENTIFIED BY "$MYSQL_DB_PASS";
+GRANT ALL PRIVILEGES ON hina_db.* TO "hina"@"phpmyadmin.srcs_my_network" IDENTIFIED BY "$MYSQL_DB_PASS";
 FLUSH PRIVILEGES;
 EOF
 sleep 3
